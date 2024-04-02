84ac58337d69ae8c7358b4dea3e6321d978b4ba2
taoning <taoningw@gmail.com>
2024-04-02 10:44:03 -0700

    update

--- README.md
+++ README.md
@@ -1,12 +1,70 @@
 # genlrtsky
 A command-line tool that uses libradtran to generate spectral sky description for Radiance ray-tracing engine.
 
+The program outputs a sky defined by a Radiance hyperspectra (.hsr) fisheye image with a spectral range of 380-780nm at 20nm interval.
+
 ## Requirements
 1. [libradtran](https://www.libradtran.org/doku.php?id=start)
 2. [Optical properties of clouds and OPAC aerosol in netcdf format.](https://www.libradtran.org/doku.php?id=download)
 
 ## Usage
+The command-line interface of `genlrtsky` follows the patterns of other
+sky generators in Radiance, such as `gensky` and `gendaylit`.
+The interface is four positional arguments followed by optional arguments:
+
+Usage: genlrtsky month day hour minute [options]
+
+Options:
+  -a float
+        Latitude, north positive (default 37)
+  -c float
+        Cloud cover 0-1
+  -d float
+        Aerosol optical depth
+  -g float
+        Albedo (default 0.2)
+  -m int
+        Standard meridian, west positive (default 120)
+  -o float
+        Longitude, west positive (default 122)
+  -p string
+        Output file prefix (default "default_")
+  -quiet
+        Quiet mode
+  -s string
+        Standard aerosol profile name (default "ca")
+  -version
+        Print version
+  -y int
+        Year (default 2003)
+
+Positional arguments:
+  month  Specify the month (1-12)
+  day    Specify the day (1-31)
+  hour   Specify the hour (0-23)
+  minute Specify the minute (0-59)
+
+### Aerosol
+We uses the standard aerosol profiles provided by the OPAC packages.
+To use one of the profile, include flag `-s`, followed by the shorthanded names, as seen in parentheses below:
+
+* continental_average (ca)
+* continental_clean (cc)
+* continental_polluted (cp)
+* maritime_clear (mc)
+* maritime_tropical (mt)
+* maritime_polluted (mp)
+* antarctic (a)
+* urban (u)
+* desert (d)
+* desert_spheroids (ds)
+
+There is also a way to scale the aerosol profile to single broadband aerosol optical depth value.
+For example, we can include `-d 0.15` flag to scale the profile to 0.15. 
+
 
 ## Examples
 
+Generate a winter solstice noon clear sky with `continental_average` standard aerosol profile
+`genlrtsky 12 21 12 0 -a 37 -o 122 -m 120 -s cc`
 
--- main.go
+++ main.go
@@ -14,40 +14,55 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"sync/atomic"
 	"text/template"
 )
 
-// Manuanlly defined constants
-const NSSAMP = 20
-const WVLSPAN = 400 // Wavelength span in nm 780 - 380
-const XRES = 64
-const YRES = 64
+const (
+	NSSAMP           = 20
+	WVLSPAN          = 400 // Wavelength span in nm 780 - 380
+	XRES             = 32
+	YRES             = 32
+	PROGRESSBARWIDTH = 50
+	TOTALSTEPS       = YRES * XRES
+)
+
+type SimulationControl struct {
+	Prefix  string
+	Version bool
+	Quiet   bool
+}
 
 type InputParams struct {
-	LibPath            string
-	SolFile            string
-	GroundAlbedo       float64
-	SolarZenith        string
-	SolarAzimuth       string
-	AerosolProfileName string
-	CosTheta           float64
-	Phi                float64
-	WavelengthGridFile string
-	Output             string
+	LibPath             string
+	SolFile             string
+	GroundAlbedo        float64
+	SolarZenith         string
+	SolarAzimuth        string
+	AerosolProfile      string
+	AerosolOpticalDepth float64
+	AodOverride         bool
+	CosTheta            float64
+	Phi                 float64
+	CloudCover          float64
+	CloudFile           string
+	WavelengthGridFile  string
+	Output              string
+	Quiet               bool
 }
 
 type Datetime struct {
-	year   int
-	month  int
-	day    int
-	hour   int
-	minute int
+	Year   int
+	Month  int
+	Day    int
+	Hour   int
+	Minute int
 }
 
 type Location struct {
-	latitude         float64
-	longitude        float64
-	standardMeridian int
+	Latitude         float64
+	Longitude        float64
+	StandardMeridian int
 }
 
 type Result struct {
@@ -62,15 +77,11 @@ func panicError(e error) {
 	}
 }
 
-func floatsToString(floats []float64) string {
-	var builder strings.Builder
-	for i, f := range floats {
-		if i > 0 {
-			builder.WriteRune(' ')
-		}
-		builder.WriteString(strconv.FormatFloat(f, 'f', -1, 64))
+func handleError(e error) {
+	if e != nil {
+		fmt.Println(e)
+		os.Exit(1)
 	}
-	return builder.String()
 }
 
 func parseMonth(str string) (int, error) {
@@ -117,30 +128,40 @@ func parseMinute(str string) (int, error) {
 	return minutes, nil
 }
 
-func parseAerosolProfile(str string) (string, error) {
-	switch str {
+func parseAerosolProfile(input *InputParams) error {
+	switch input.AerosolProfile {
 	case "ca":
-		return "continental_average", nil
+		input.AerosolProfile = "continental_average"
+		return nil
 	case "cc":
-		return "continental_clean", nil
+		input.AerosolProfile = "continental_clean"
+		return nil
 	case "cp":
-		return "continental_polluted", nil
+		input.AerosolProfile = "continental_polluted"
+		return nil
 	case "mc":
-		return "maritime_clear.dat", nil
+		input.AerosolProfile = "maritime_clear"
+		return nil
 	case "mt":
-		return "maritime_tropical.dat", nil
+		input.AerosolProfile = "maritime_tropical"
+		return nil
 	case "mp":
-		return "maritime_polluted.dat", nil
+		input.AerosolProfile = "maritime_polluted"
+		return nil
 	case "a":
-		return "antarctic", nil
+		input.AerosolProfile = "antarctic"
+		return nil
 	case "u":
-		return "urban", nil
+		input.AerosolProfile = "urban"
+		return nil
 	case "d":
-		return "desert", nil
+		input.AerosolProfile = "desert"
+		return nil
 	case "ds":
-		return "desert_spheroids", nil
+		input.AerosolProfile = "desert_spheroids"
+		return nil
 	default:
-		return "", errors.New("Invalid aerosol profile, use one of these: ca, cc, cp, mc, mt, mp, a, u, d, ds")
+		return errors.New("Invalid aerosol profile, use one of these: ca, cc, cp, mc, mt, mp, a, u, d, ds")
 	}
 }
 
@@ -160,27 +181,27 @@ func printVersion() {
 	if err != nil {
 		fmt.Println("zenith not found")
 	}
-	os.Exit(0)
+	return
 }
 
-func getSolarAngle(dt Datetime, lc Location) (string, string) {
-	cmd := exec.Command("zenith", "-y", strconv.Itoa(dt.year),
-		"-a", strconv.FormatFloat(lc.latitude, 'f', -1, 64),
-		"-o", strconv.FormatFloat(lc.longitude, 'f', -1, 64),
-		"-s", strconv.Itoa(lc.standardMeridian),
-		strconv.Itoa(dt.day), strconv.Itoa(dt.month),
-		strconv.Itoa(dt.hour), strconv.Itoa(dt.minute),
+func getSolarAngle(input *InputParams, dt *Datetime, lc *Location) error {
+	cmd := exec.Command("zenith", "-q", "-y", strconv.Itoa(dt.Year),
+		"-a", strconv.FormatFloat(lc.Latitude, 'f', -1, 64),
+		"-o", strconv.FormatFloat(lc.Longitude, 'f', -1, 64),
+		"-s", strconv.Itoa(lc.StandardMeridian),
+		strconv.Itoa(dt.Day), strconv.Itoa(dt.Month),
+		strconv.Itoa(dt.Hour), strconv.Itoa(dt.Minute),
 	)
 	var stdoutBuf, stderrBuf bytes.Buffer
 	cmd.Stdout = &stdoutBuf
 	cmd.Stderr = &stderrBuf
 
-	// fmt.Println(cmd.Args)
 	err := cmd.Run()
-	panicError(err)
-	// stdout: 12:00:00   60.4563  358.4449
+	fmt.Fprintf(os.Stderr, stderrBuf.String())
 	solarAngles := strings.Fields(stdoutBuf.String())
-	return solarAngles[1], solarAngles[2]
+	input.SolarZenith = solarAngles[1]
+	input.SolarAzimuth = solarAngles[2]
+	return err
 }
 
 func pix2loc(xres int, yres int, px int, py int) (float64, float64) {
@@ -272,15 +293,11 @@ func compute(x, y int, input InputParams, tmpl *template.Template) ([NSSAMP + 1]
 	input.CosTheta = umu
 	input.Phi = phi
 	err := tmpl.Execute(&templateBuffer, input)
-	if err != nil {
-		panic(err)
-	}
+	panicError(err)
 	inputStr := templateBuffer.String()
 
 	tdir, err := os.MkdirTemp("", "")
-	if err != nil {
-		panic(err)
-	}
+	panicError(err)
 	defer os.RemoveAll(tdir)
 
 	// fmt.Println(inputStr)
@@ -294,13 +311,9 @@ func compute(x, y int, input InputParams, tmpl *template.Template) ([NSSAMP + 1]
 	cmd.Stdout = &out
 
 	err = cmd.Run()
-	if err != nil {
-		panic(err)
-	}
+	panicError(err)
 	res, err := os.ReadFile(filepath.Join(tdir, "mc.rad.spc"))
-	if err != nil {
-		panic(err)
-	}
+	panicError(err)
 	rows := strings.Split(string(res), "\n")
 	scolor := [20]float64{}
 	for i, row := range rows {
@@ -309,75 +322,28 @@ func compute(x, y int, input InputParams, tmpl *template.Template) ([NSSAMP + 1]
 		}
 		fields := strings.Fields(row)
 		value, err := strconv.ParseFloat(fields[4], 64)
-		if err != nil {
-			panic(err)
-		}
+		panicError(err)
 		scolor[i] = value * WVLSPAN / 1000 // mW to W
 	}
 	sclr := scolor2scolr(scolor, NSSAMP)
-	fmt.Println(x, y)
 	return sclr, err
 }
 
-func main() {
+func getSunDirection(input *InputParams) [3]float64 {
+	sundir := [3]float64{}
+	theta, err := strconv.ParseFloat(input.SolarZenith, 64)
+	theta *= math.Pi / 180.0
+	panicError(err)
+	phi, err := strconv.ParseFloat(input.SolarAzimuth, 64)
+	phi = (270 - phi) * math.Pi / 180.0
+	panicError(err)
+	sundir[0] = math.Sin(theta) * math.Cos(phi)
+	sundir[1] = math.Sin(theta) * math.Sin(phi)
+	sundir[2] = math.Cos(theta)
+	return sundir
+}
 
-	inputTemplate := `data_files_path {{.LibPath}} 
-source solar {{.SolFile}}
-rte_solver mystic
-albedo {{.GroundAlbedo}}
-sza {{.SolarZenith}}
-phi0 {{.SolarAzimuth}}
-aerosol_default
-aerosol_species_file {{.AerosolProfileName}}
-umu {{.CosTheta}}
-phi {{.Phi}}
-wavelength_grid_file {{.WavelengthGridFile}}
-mol_abs_param crs
-mc_spherical 1D
-mc_vroom on`
-
-	tmpl, err := template.New("input").Parse(inputTemplate)
-	if err != nil {
-		panic(err)
-	}
-
-	input := InputParams{}
-
-	// Extraterrestrial solar irradiance in mW/m^2/nm
-	EXTSOL := [NSSAMP]float64{1192.74, 1772.2, 1394.52, 2069.67,
-		2074.3, 1964.5, 1971.29, 1942.59, 1870.53,
-		1852.63, 1772.84, 1737.29, 1662.2, 1586.51,
-		1537.19, 1475.65, 1400.21, 1320.18, 1273.23,
-		1220.45,
-	}
-
-	libPath := os.Getenv("LIBRADTRAN_DATA_FILES")
-	if libPath == "" {
-		fmt.Fprintln(os.Stderr, "LIBRADTRAN_DATA_FILES not set")
-		return
-	}
-	input.LibPath = libPath
-
-	// Command-line flags
-	year := flag.Int("y", 2003, "Year")
-	latitude := flag.Float64("a", 37.0, "latitude")
-	longitude := flag.Float64("o", 122.0, "longitude")
-	standardMeridian := flag.Int("m", 120, "Standard meridian, west positive")
-	version := flag.Bool("version", false, "Print version")
-	albedo := flag.Float64("g", 0.2, "Albedo")
-	aerosolProfile := flag.String("ap", "continental_average", "Standard aerosol profile name")
-	prefix := flag.String("p", "Default", "Output file prefix")
-	// cloudCover := flag.String("cloud cover", "continental_average", "Standard aerosol profile name")
-
-	input.GroundAlbedo = *albedo
-	input.AerosolProfileName = *aerosolProfile
-
-	flag.Parse()
-
-	if *version {
-		printVersion()
-	}
-
+func parsePositionalArgs(datetime *Datetime) {
 	// Check and get positional arguments before any optional flags.
 	positionalArgs := []string{}
 	for _, arg := range os.Args[1:] { // Skipping the program name.
@@ -389,49 +355,163 @@ mc_vroom on`
 
 	// Make sure we got the first three positional arguments.
 	if len(positionalArgs) < 4 {
-		fmt.Fprintf(os.Stderr, "Usage: genlrtsky month day hour minute [options]\n")
+		flag.Usage()
 		os.Exit(0)
 	}
 
 	month, err := parseMonth(positionalArgs[0])
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		return
-	}
+	handleError(err)
 	day, err := parseDay(positionalArgs[1])
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		return
-	}
+	handleError(err)
 	hour, err := parseHour(positionalArgs[2])
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		return
-	}
+	handleError(err)
 	minute, err := parseMinute(positionalArgs[3])
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		return
+	handleError(err)
+	datetime.Month = month
+	datetime.Day = day
+	datetime.Hour = hour
+	datetime.Minute = minute
+}
+
+func setupFlags(simctrl *SimulationControl, input *InputParams, dateTime *Datetime, location *Location) {
+	flag.IntVar(&dateTime.Year, "y", 2003, "Year")
+	flag.Float64Var(&location.Latitude, "a", 37.0, "Latitude, north positive")
+	flag.Float64Var(&location.Longitude, "o", 122.0, "Longitude, west positive")
+	flag.IntVar(&location.StandardMeridian, "m", 120, "Standard meridian, west positive")
+	flag.Float64Var(&input.GroundAlbedo, "g", 0.2, "Albedo")
+	flag.StringVar(&input.AerosolProfile, "s", "ca", "Standard aerosol profile name")
+	flag.Float64Var(&input.AerosolOpticalDepth, "d", 0.0, "Aerosol optical depth")
+	flag.StringVar(&simctrl.Prefix, "p", "default_", "Output file prefix")
+	flag.BoolVar(&simctrl.Version, "version", false, "Print version")
+	flag.Float64Var(&input.CloudCover, "c", 0.0, "Cloud cover 0-1")
+	flag.BoolVar(&simctrl.Quiet, "quiet", false, "Quiet mode")
+
+	// Customize the usage function
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: genlrtsky month day hour minute [options]\n")
+		flag.PrintDefaults()
+		fmt.Fprintln(os.Stderr, "\nPositional arguments:")
+		fmt.Fprintln(os.Stderr, "  month  Specify the month (1-12)")
+		fmt.Fprintln(os.Stderr, "  day    Specify the day (1-31)")
+		fmt.Fprintln(os.Stderr, "  hour   Specify the hour (0-23)")
+		fmt.Fprintln(os.Stderr, "  minute Specify the minute (0-59)")
 	}
+}
+
+func getLibPathOrExit() string {
+	libPath := os.Getenv("LIBRADTRAN_DATA_FILES")
+	if libPath == "" {
+		fmt.Fprintln(os.Stderr, "LIBRADTRAN_DATA_FILES not set")
+		os.Exit(1)
+	}
+	return libPath
+}
+
+func displayProgressBar(completedSteps int32, totalSteps int, progressBarWidth int) {
+	progress := float64(completedSteps) / float64(totalSteps)
+	bar := strings.Repeat("=", int(progress*float64(progressBarWidth)))
+	fmt.Printf("\r[%-50s] %d%%", bar, int(progress*100))
+}
+
+func writeHsr(hsrf *os.File, hsr *bytes.Buffer) {
+	hsrf.WriteString("#?RADIANCE\n")
+	fmt.Fprintf(hsrf, "NCOMP=%d\n", NSSAMP)
+	hsrf.WriteString("WAVELENGTH_SPLITS= 380 480 588 780\n")
+	hsrf.WriteString("FORMAT=Radiance_spectra\n\n")
+	fmt.Fprintf(hsrf, "-Y       %d +X       %d\n", YRES, XRES)
+	hsrf.Write(hsr.Bytes())
+}
+
+func main() {
+
+	completedSteps := int32(0)
+
+	inputTemplate := `data_files_path {{.LibPath}} 
+source solar {{.SolFile}}
+rte_solver mystic
+albedo {{.GroundAlbedo}}
+sza {{.SolarZenith}}
+phi0 {{.SolarAzimuth}}
+aerosol_default
+aerosol_species_file {{.AerosolProfile}}
+{{if .AodOverride}}aerosol_modify tau set {{.AerosolOpticalDepth}}{{end}}
+{{if .CloudCover}}
+wc_file 1D {{.CloudFile}}
+cloudcover wc {{.CloudCover}}
+interpret_as_level wc
+{{end}}
+umu {{.CosTheta}}
+phi {{.Phi}}
+wavelength_grid_file {{.WavelengthGridFile}}
+mol_abs_param crs
+mc_spherical 1D
+mc_vroom on
+{{if .Quiet}}quiet{{end}}`
+
+	tmpl, err := template.New("input").Parse(inputTemplate)
+	panicError(err)
+
+	input := InputParams{}
+
+	// Extraterrestrial solar irradiance in mW/m^2/nm
+	EXTSOL := [NSSAMP]float64{1192.74, 1772.2, 1394.52, 2069.67,
+		2074.3, 1964.5, 1971.29, 1942.59, 1870.53,
+		1852.63, 1772.84, 1737.29, 1662.2, 1586.51,
+		1537.19, 1475.65, 1400.21, 1320.18, 1273.23,
+		1220.45,
+	}
+
+	input.LibPath = getLibPathOrExit()
+	dateTime := Datetime{}
+	location := Location{}
+	simctrl := SimulationControl{}
+
+	setupFlags(&simctrl, &input, &dateTime, &location)
+	parsePositionalArgs(&dateTime)
+
+	os.Args = os.Args[4:]
+	flag.Parse()
+
+	if simctrl.Version {
+		printVersion()
+		os.Exit(0)
+	}
+
+	err = parseAerosolProfile(&input)
+	handleError(err)
+	input.AodOverride = false
+	if input.AerosolOpticalDepth > 0 {
+		input.AodOverride = true
+	}
+
+	if simctrl.Quiet {
+		input.Quiet = true
+	}
+
+	outRad := simctrl.Prefix + "sky.rad"
+	outHsr := simctrl.Prefix + "sky.hsr"
+
+	hsrf, err := os.Create(outHsr)
+	handleError(err)
+	defer hsrf.Close()
+
+	radf, err := os.Create(outRad)
+	handleError(err)
+	defer radf.Close()
 
 	// Get the solar angle
-	dt := Datetime{year: *year, month: month, day: day, hour: hour, minute: minute}
-	lc := Location{latitude: *latitude, longitude: *longitude, standardMeridian: *standardMeridian}
-	solarZenith, solarAzimuth := getSolarAngle(dt, lc)
-	input.SolarZenith = solarZenith
-	input.SolarAzimuth = solarAzimuth
+	err = getSolarAngle(&input, &dateTime, &location)
+	panicError(err)
 
 	// Create a temp file to store solar and wavelength data
-	wavelengthGridFile, err := os.CreateTemp("", "lambda.dat")
+	wavelengthGridFile, err := os.CreateTemp("", "lambda")
 	panicError(err)
 	defer os.Remove(wavelengthGridFile.Name())
 	defer wavelengthGridFile.Close()
 	input.WavelengthGridFile = wavelengthGridFile.Name()
 
-	solFile, err := os.CreateTemp("", "solar.dat")
-	if err != nil {
-		panic(err)
-	}
+	solFile, err := os.CreateTemp("", "solar")
+	panicError(err)
 	defer os.Remove(solFile.Name())
 	defer solFile.Close()
 	input.SolFile = solFile.Name()
@@ -441,6 +521,21 @@ mc_vroom on`
 		wavelengthStr := strconv.Itoa(wavelength)
 		wavelengthGridFile.WriteString(wavelengthStr + "\n")
 		solFile.WriteString(wavelengthStr + " " + strconv.FormatFloat(value, 'f', -1, 64) + "\n")
+	}
+
+	if input.CloudCover > 0 {
+		cloudFile, err := os.CreateTemp("", "cloud")
+		panicError(err)
+		defer os.Remove(cloudFile.Name())
+		defer cloudFile.Close()
+		cloudFile.WriteString(`
+		5.0	0	0
+		4.0	0.25	10.0
+		3.0	0.25	10.0
+		2.0	0.25	10.0
+		1.0	0	0.0
+		0.0	0	0.0`)
+		input.CloudFile = cloudFile.Name()
 	}
 	results := make(chan Result, YRES*XRES)
 	wg := &sync.WaitGroup{}
@@ -454,6 +549,10 @@ mc_vroom on`
 			concurrencyLimit <- struct{}{} // Acquire a "slot"
 			wg.Add(1)
 			go worker(x, y, input, tmpl, wg, concurrencyLimit, results)
+			atomic.AddInt32(&completedSteps, 1)
+			if !simctrl.Quiet {
+				displayProgressBar(completedSteps, TOTALSTEPS, PROGRESSBARWIDTH)
+			}
 		}
 	}
 
@@ -469,7 +568,6 @@ mc_vroom on`
 	}
 
 	for res := range results {
-		fmt.Println(res.X, res.Y)
 		if res.Err != nil {
 			fmt.Printf("Error processing (%d, %d): %v\n", res.X, res.Y, res.Err)
 			continue
@@ -483,30 +581,65 @@ mc_vroom on`
 		for x := range orderedResults[y] {
 			if res := orderedResults[y][x]; res.Err == nil {
 				for _, value := range res.Data {
-					if err := binary.Write(&hsr, binary.LittleEndian, value); err != nil {
-						panic(err)
-					}
+					err := binary.Write(&hsr, binary.LittleEndian, value)
+					panicError(err)
 				}
 			}
 		}
 	}
 
-	outRad := *prefix + "sky.rad"
-	outHsr := *prefix + "sky.hsr"
-	hsrf, err := os.Create(outHsr)
-	defer hsrf.Close()
-	hsrf.WriteString("#?RADIANCE\n")
-	hsrf.WriteString("NCOMP=20\n")
-	hsrf.WriteString("WAVELENGTH_SPLITS= 380 480 588 780\n")
-	hsrf.WriteString("FORMAT=Radiance_spectra\n\n")
-	resStr := fmt.Sprintf("-Y       %d +X       %d\n", YRES, XRES)
-	hsrf.WriteString(resStr)
-	hsrf.Write(hsr.Bytes())
+	var templateBuffer bytes.Buffer
+	input.CosTheta = -1
+	input.Phi = 0
+	err = tmpl.Execute(&templateBuffer, input)
+	panicError(err)
+	inputStr := templateBuffer.String()
+	inputStr += "\n" + "output_user edir"
+	inputStr += "\n" + "rte_solver disort"
 
-	radf, err := os.Create(outRad)
-	defer radf.Close()
-	skyfuncStr := fmt.Sprintf("void specpict skyfunc 9 noop %s fisheye.cal fish_u fish_v -rx 90 -rz 90 0 0\n", outHsr)
-	radf.WriteString(skyfuncStr)
+	tdir, err := os.MkdirTemp("", "")
+	panicError(err)
+	defer os.RemoveAll(tdir)
+
+	// fmt.Println(inputStr)
+	cmd := exec.Command("uvspec")
+	cmd.Stdin = bytes.NewBufferString(inputStr)
+	cmd.Dir = tdir
+
+	// Capture the output
+	var stderr, out bytes.Buffer
+	cmd.Stderr = &stderr
+	cmd.Stdout = &out
+
+	err = cmd.Run()
+	panicError(err)
+	sun_radiance := [NSSAMP]float64{}
+	for idx, value := range strings.Split(strings.Trim(out.String(), " \n"), "\n") {
+		sun_radiance[idx], err = strconv.ParseFloat(strings.TrimSpace(value), 64)
+		panicError(err)
+	}
+
+	sundir := getSunDirection(&input)
+	writeHsr(hsrf, &hsr)
+
+	fmt.Fprintf(radf, "# Latitude: %f, Longitude: %f, TimeZone: %d\n", location.Latitude, location.Longitude, location.StandardMeridian)
+	fmt.Fprintf(radf, "# Date time: %d-%d-%d %d:%02d\n", dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, dateTime.Minute)
+	fmt.Fprintf(radf, "# Solar zenith and azimuth: %s, %s\n", input.SolarZenith, input.SolarAzimuth)
+	fmt.Fprintf(radf, "# Ground albedo: %f\n", input.GroundAlbedo)
+	fmt.Fprintf(radf, "# Aerosol profile: %s\n", input.AerosolProfile)
+	if input.AodOverride {
+		fmt.Fprintf(radf, "# Overriding aerosol optical depth: %f\n", input.AerosolOpticalDepth)
+	}
+	if input.CloudCover > 0 {
+		fmt.Fprintf(radf, "# Cloud cover: %f\n", input.CloudCover)
+	}
+	radf.WriteString("void spectrum sun\n0\n0\n22 380 780 ")
+	for _, value := range sun_radiance {
+		fmt.Fprintf(radf, "%.1f ", value*WVLSPAN/6.7967e-5/1000/sundir[2])
+	}
+	radf.WriteString("\n\nsun light solar\n0\n0\n3 1 1 1\n\n")
+	fmt.Fprintf(radf, "solar source sun\n0\n0\n4 %f %f %f 0.533\n\n", sundir[0], sundir[1], sundir[2])
+	fmt.Fprintf(radf, "void specpict skyfunc 9 noop %s fisheye.cal fish_u fish_v -rx 90 -rz 90 0 0\n", outHsr)
 	radf.WriteString("skyfunc glow skyglow 0 0 4 1 1 1 0\n")
 	radf.WriteString("skyglow source sky 0 0 4 0 0 1 180\n")
 }
